c7ac021dee4af18f3420958f0e338703286b07d1
Marc Donald <marcsdeveloperemail@gmail.com>
2020-12-12 13:52:35 +0000

    Update README instructions to use `bash` instead of `sh`
    Also some minor formatting changes

--- mic-volume/mic-volume.sh
+++ mic-volume/mic-volume.sh
@@ -1,25 +1,22 @@
 #!/bin/sh
 
-if [ -z "$1" ]
-then
+if [ -z "$1" ]; then
   volume=$(pacmd list-sources | grep "\* index:" -A 7 | grep "volume" | awk -F/ '{print $2}')
   mute=$(pacmd list-sources | grep "\* index:" -A 11 | grep "muted")
 else
   volume=$(pacmd list-sources | grep "$1" -A 6 | grep "volume" | awk -F/ '{print $2}')
-  mute=$(pacmd list-sources | grep "$1" -A 11 | grep "muted" ) # didn't test this one
+  mute=$(pacmd list-sources | grep "$1" -A 11 | grep "muted" )
 fi
 
-if [ -z "$volume" ]
-then
+if [ -z "$volume" ]; then
   echo "No Mic Found"
 else
-volume="${volume//[[:blank:]]/}" 
- if [[ "$mute" == *"yes"* ]]; then
-       	echo "[$volume]"
-
- elif [[ "$mute" == *"no"* ]]; then
-       	echo "$volume"
- else
-	echo "$volume !"
- fi
+  volume="${volume//[[:blank:]]/}" 
+  if [[ "$mute" == *"yes"* ]]; then
+    echo "[$volume]"
+  elif [[ "$mute" == *"no"* ]]; then
+    echo "$volume"
+  else
+    echo "$volume !"
+  fi
 fi
