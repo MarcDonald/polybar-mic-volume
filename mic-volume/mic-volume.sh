eb62c594b4b21209194fe6e66239f67fd46cc358
Loxymondor <mikosimo0@gmail.com>
2020-12-12 11:19:03 +0200

    adding mute

--- mic-volume/mic-volume.sh
+++ mic-volume/mic-volume.sh
@@ -3,13 +3,15 @@
 if [ -z "$1" ]
 then
   volume=$(pacmd list-sources | grep "\* index:" -A 7 | grep "volume" | awk -F/ '{print $2}')
+  mute=$(pacmd list-sources | grep "\* index:" -A 11 | grep "muted" | awk -F/ '{print $2}')
 else
   volume=$(pacmd list-sources | grep "$1" -A 6 | grep "volume" | awk -F/ '{print $2}')
+  mute=$(pacmd list-sources | grep "$1" -A 11 | grep "muted" | awk -F/ '{print $2}')
 fi
 
 if [ -z "$volume" ]
 then
   echo "No Mic Found"
 else
-  echo "$volume"
+  echo "$volume $mute"
 fi
